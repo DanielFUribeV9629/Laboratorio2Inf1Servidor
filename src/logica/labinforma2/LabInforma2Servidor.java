e2734e54de6065400ffcf78ee033d162c0a9d8a6
DanielFUribeV9629 <danifelpo@hotmail.com>
2021-11-09 19:23:36 -0500

    Estructura de Servidor V2

---
+++ src/logica/labinforma2/LabInforma2Servidor.java
@@ -0,0 +1,15 @@
+package logica.labinforma2;
+
+import presentacion.vista.Vista;
+
+public class LabInforma2Servidor {
+
+    public static void main(String[] args) {
+        //Paginacion mod= new Paginacion();
+        Vista view = new Vista();       
+        //ControladorParticion control= new ControladorParticion(view, mod);
+        //control.iniciar();
+        view.setVisible(true);   
+    } 
+    
+}
